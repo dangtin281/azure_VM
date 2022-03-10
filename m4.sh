bfc10586da982a47bf15a234e4dfaf54913510c6
dangtin281 <86880836+dangtin281@users.noreply.github.com>
2022-03-10 22:17:28 +0700

    Update m4.sh

--- m4.sh
+++ m4.sh
@@ -2,4 +2,8 @@
 
 gitpath=$(head -1 gitpath.txt)
 wget https://raw.githubusercontent.com/$gitpath/main/createvm.sh
+wget https://raw.githubusercontent.com/$gitpath/main/vmcreate.sh
+wget https://raw.githubusercontent.com/$gitpath/main/list_region_createvm.txt
+
 chmod +x createvm.sh
+chmod +x vmcreate.sh
