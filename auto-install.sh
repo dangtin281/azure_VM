9180eb0be5c78f6516499eab54956bfceb0a633a
dangtin281 <86880836+dangtin281@users.noreply.github.com>
2022-03-11 11:44:36 +0700

    Update auto-install.sh

--- auto-install.sh
+++ auto-install.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
-echo "awsdiami/CliAzureGit" > gitpath.txt
+echo "dangtin281/azure_VM" > gitpath.txt
 curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
 echo "Truy cap URL https://microsoft.com/devicelogin, sau do gan code ben duoi de approve cli access"
 az login
 read -p "Copy Code ben tren & Paste vao url vua mo, Bam phim bat ky de sang buoc tiep theo " step1
 
 wget https://raw.githubusercontent.com/awsdiami/AzureCLN/main/auto-start.sh
-chmod +x auto-start.sh
+wget https://raw.githubusercontent.com/awsdiami/CliAzureGit/main/cmdviewgroup.sh
+
+chmod +x auto-start.sh 
+chmod +x cmdviewgroup.sh
 ./auto-start.sh
 
 echo "Kiem tra crontab"
@@ -15,4 +18,3 @@ crontab -l
 
 echo "Kiem tra xem da chay chua ?"
 echo "tail -f run.log"
-
