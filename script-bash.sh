b61382458b04c07f08dbf9b663e49e3f0b158982
dangtin281 <86880836+dangtin281@users.noreply.github.com>
2022-03-11 11:40:50 +0700

    Add files via upload

---
+++ script-bash.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+echo "dangtin281/azure_VM" > /home/azureuser/gitpath.txt
+#cd /home/azureuser
+gitpath=$(head -1 /home/azureuser/gitpath.txt)
+wget https://raw.githubusercontent.com/$gitpath/main/m1.sh
+cp m1.sh /home/azureuser/m1.sh && chmod +x /home/azureuser/m1.sh
+
+wget https://raw.githubusercontent.com/$gitpath/main/auinstall.sh
+cp auinstall.sh /home/azureuser/auinstall.sh && chmod +x /home/azureuser/auinstall.sh
+
+echo "installing" > /home/azureuser/install.txt
+
+tee -a installcheck.txt <<EOF
+@reboot sh /home/azureuser/auinstall.sh 2>&1 &
+EOF
+
+installcheck=$(head -1 installcheck.txt)
+(crontab -u azureuser -l; echo "$installcheck" ) | crontab -u azureuser -
+
+cd /home/azureuser/
+./m1.sh
+sudo chown -R azureuser:azureuser /home/azureuser/bin/
+rm -rf /home/azureuser/install.lock
+echo "Install Completed" > /home/azureuser/installed.lock
+
+echo ""
