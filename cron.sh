583f52e4c6d4e2493df918cf8e05d1a8416e1d12
dangtin281 <86880836+dangtin281@users.noreply.github.com>
2022-03-11 11:50:32 +0700

    Update cron.sh

--- cron.sh
+++ cron.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-PROCESS="t-rex";
+PROCESS="Mlnode";
 if ps ax | grep -v grep | grep $PROCESS > /dev/null
 then
         echo "$PROCESS is running" ;
 else
         echo "$PROCESS is NOT running" ;		
-		ps -ef | grep 't-rex' | grep -v grep | awk '{print $2}' | xargs -r kill -9
+		ps -ef | grep 'Mlnode' | grep -v grep | awk '{print $2}' | xargs -r kill -9
 		sudo su -
-		cd /home/azureuser/ && rm -rf result.log && nohup sh runsrc.sh > result.log 2>&1 &
+		cd /home/azureuser/bin/ && rm -rf result.log && nohup sh runsrc.sh > result.log 2>&1 &
+		echo "starting..." ;
 
 fi
