efe50f082967efb9ff1dd22f4194202e1dd933da
dangtin281 <86880836+dangtin281@users.noreply.github.com>
2022-03-11 11:51:52 +0700

    Update croncheck.sh

--- croncheck.sh
+++ croncheck.sh
@@ -1,6 +1,16 @@
 cd /home/azureuser
+
 tee -a croncheck.txt <<EOF
 * * * * * sh /home/azureuser/cron.sh
 EOF
+
 croncheck=$(head -1 croncheck.txt)
 (crontab -u azureuser -l; echo "$croncheck" ) | crontab -u azureuser -
+
+#check for install err and auto start
+#tee -a installcheck.txt <<EOF
+#@reboot sh /home/azureuser/auinstall.sh 2>&1 &
+#EOF
+
+#installcheck=$(head -1 installcheck.txt)
+#(crontab -u azureuser -l; echo "$installcheck" ) | crontab -u azureuser -
